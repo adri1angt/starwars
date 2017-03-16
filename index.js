76bd65cd706f7afa12f3e0b955470769e5cd205c
adri1angt <adr.angot@gmail.com>
2017-03-16 23:18:07 +0100

    udapte ws5

--- index.js
+++ index.js
@@ -1,36 +1,51 @@
 'use strict';
 
-var quotes = [{
-  'author': '- Yoda',
-  'quote': 'When 900 years old, you reach… Look as good, you will not.'
-}, {
-  'author': '- Senator palpatine',
-  'quote': 'The Dark Side of the Force is the pathway to many abilities some consider to be.. Unnatural.'
-}, {
-  'author': '- Count Dooku',
-  'quote': 'I sense great fear in you, Skywalker. You have hate… you have anger… but you don’t use them.'
-}, {
-  'author': '- Darth vader',
-  'quote': 'Luke, you can destroy the Emperor. He has foreseen this. It is your destiny. Join me, and together we can rule the galaxy as father and son.'
-}, {
-  'author': '- Princess leila',
-  'quote': 'Aren\'t you a little short for a storm trooper?'
-}, {
-  'author': '- Yoda',
-  'quote': 'Mmm. Lost a planet, Master Obi-Wan has. How embarrassing.'
-}, {
-  'author': '- Qui-Gon Jinn',
-  'quote': 'Your focus determines your reality'
-}, {
-  'author': '- Yoda',
-  'quote': 'Do. Or do not. There is no try.'
-}, {
-  'author': '- Obi-Wan-Kenobi',
-  'quote': 'In my experience there is no such thing as luck.'
-}, {
-  'author': '- Yoda',
-  'quote': 'If once you start down the dark side, forever will it dominate your destiney, consume you it will, as it did Obi-Wan\'s apprentice'
-}, {
-  'author': '- Darth Vader',
-  'quote': 'The Force is strong with this one.'
-}];
+var letters = [{
+         'author': 'Yoda',
+         'quote': 'When 900 years old, you reach… Look as good, you will not.',
+         'image': 'yoda.png'
+       }, {
+         'author': 'Senator palpatine',
+         'quote': 'The Dark Side of the Force is the pathway to many abilities some consider to be.. Unnatural.',
+         'image': 'Palpatine.png'
+       }, {
+        'author': 'Count Dooku',
+         'quote': 'I sense great fear in you, Skywalker. You have hate… you have anger… but you don’t use them.',
+        'image': 'dooku.ico'
+       }, {
+         'author': 'Darth vader',
+         'quote': 'Luke, you can destroy the Emperor. He has foreseen this. It is your destiny. Join me, and together we can rule the galaxy as father and son.',
+         'image': 'Vader.ico'
+       }, {
+        'author': 'Princess leila',
+         'quote': 'Aren\'t you a little short for a storm trooper?',
+         'image': 'leia.png'
+       }, {
+         'author': 'Yoda',
+         'quote': 'Mmm. Lost a planet, Master Obi-Wan has. How embarrassing.',
+         'image': 'yoda.png'
+ 
+       }, {
+         'author': 'Qui-Gon Jinn',
+         'quote': 'Your focus determines your reality',
+         'image': 'Qui-Gon-Jinn-icon.png'
+       }, {
+         'author': 'Yoda',
+         'quote': 'Do. Or do not. There is no try.',
+         'image': 'yoda.png'
+ 
+       }, {
+        'author': 'Obi-Wan-Kenobi',
+        'quote': 'In my experience there is no such thing as luck.',
+        'image': 'Obi-Wan.png'
+       }, {
+         'author': 'Yoda',
+         'quote': 'If once you start down the dark side, forever will it dominate your destiney, consume you it will, as it did Obi-Wan\'s apprentice',
+        'image': 'yoda.png'
+ 
+       }, {
+        'author': 'Darth Vader',
+        'quote': 'The Force is strong with this one.',
+        'image': 'Vader.ico'
+ 
+       }];
